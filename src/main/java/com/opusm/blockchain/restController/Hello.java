252fe6edb36df784b28fcd561ce0cc5d7f7c0f4c
hahahahoho <maha4092@gmail.com>
2022-11-10 15:07:27 +0900

    project setting

---
+++ src/main/java/com/opusm/blockchain/restController/Hello.java
@@ -0,0 +1,13 @@
+package com.opusm.blockchain.restController;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hello {
+
+    @GetMapping("/")
+    public String hello(){
+        return "Hello World";
+    }
+}
